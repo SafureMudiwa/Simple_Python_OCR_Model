2f710d1b0100961318b23e5a32db987fd34bb91d
SafureMudiwa <mudiwasafure01@gmail.com>
2025-08-05 20:10:16 +0200

    Issue: Trying to write Extracted text to a TXT file.

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import pytesseract
 #when pytesseract does not find Tesseract installation automatically
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-image_path = "sampleimg1.png" #image file
+image_path = 'sampleimg1.png' #image file
 img = Image.open(image_path)
 
 extracted_text = pytesseract.image_to_string(img) #the text extract
-print("Extracted Text: ")
-print(extracted_text)
 
+with open("extracted_text.txt", "w") as file:
+    file.write(extracted_text)
+
+print("\nTEXT SUCCESSFULLY SAVED: ")
+
+with open("extract_text.txt","+r") as output:
+   display = output.read()
+
+print(display)
