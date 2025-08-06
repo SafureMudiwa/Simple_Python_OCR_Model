a38cb2813cc69a213c19651ca2a7178a935fd338
SafureMudiwa <mudiwasafure01@gmail.com>
2025-08-06 11:47:38 +0200

    Change: Auto-reads file type, User just enters file name only now.

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ import pytesseract
 #setting path for tesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-file_name = input("ENTER IMAGE NAME HERE: ")
-folder = r"images"
+file_name = input("ENTER IMAGE NAME HERE: ").strip()
 
 def open_img_auto(file_name, folder=""):
 
-    extensions = ["png", "jpg", "jpeg", "gif", "tiff", "raw"]
+    extensions = [".png", ".jpg", ".jpeg", ".gif", ".tiff"]
     for ext in extensions:
         path = os.path.join(folder, file_name + ext)
         if os.path.exists(path):
             return Image.open(path) #this line is Pillow opening the file.
-    
-    raise FileExistsError(f"{file_name} NOT FOUND IN {folder}")
+        
+    raise FileNotFoundError(f"{file_name} NOT FOUND IN {folder}")
 
-img = open_img_auto(file_name, folder)
+img = open_img_auto(file_name, folder="images")
 extracted_text = pytesseract.image_to_string(img)
 
 output_file = "extracted_text.txt"
