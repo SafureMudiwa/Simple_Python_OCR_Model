447512128a8b9576d5b846bc01df9e86d56c6377
SafureMudiwa <mudiwasafure01@gmail.com>
2025-08-06 10:33:11 +0200

    Addition: created folder for images to automate extension reading

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+import os
 from PIL import Image
 import pytesseract
 
 #setting path for tesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-image_path = "sampleimg1.png"
-img = Image.open(image_path) #opening image using Pillow
+file_name = input("ENTER IMAGE NAME HERE: ")
+folder = r"images"
+
+def open_img_auto(file_name, folder=""):
+
+    extensions = ["png", "jpg", "jpeg", "gif", "tiff", "raw"]
+    for ext in extensions:
+        path = os.path.join(folder, file_name + ext)
+        if os.path.exists(path):
+            return Image.open(path) #this line is Pillow opening the file.
+    
+    raise FileExistsError(f"{file_name} NOT FOUND IN {folder}")
+
+img = open_img_auto(file_name, folder)
 extracted_text = pytesseract.image_to_string(img)
 
 output_file = "extracted_text.txt"
